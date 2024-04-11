cbf53959370a64a96ea1be46ac1cfb931558df99
Adnantium <adnantium@gmail.com>
2024-04-11 12:22:53 -0400

    TestUpdateApplicant

--- applicants/tests.py
+++ applicants/tests.py
@@ -9,8 +9,18 @@ from rest_framework.test import (
     APIClient,
 )
 from applicants.views import ApplicantViewSet
+from applicants.models import Applicant, APPROVED, REJECTED, PENDING
+
 
 class ATSAPITestCase(APITestCase):
+    """Helper base class for ATS API tests. Sets up test users and groups and utility methods."""
+    
+    fixtures = ["groups.json"]
+    client = APIClient()
+
+    email = "email@email.com"
+    password = "test-password"
+
     def create_test_user(
         self, username: str, email: str, password: str, group_names: list = []
     ):
@@ -21,21 +31,20 @@ class ATSAPITestCase(APITestCase):
         user.save()
         return user
 
+
 class TestGetCreateApplicant(ATSAPITestCase):
     """Tests the ApplicantViewSet for GET and POST requests.
     Checks that users with the correct permissions can view and add applicants.
     Permissions tested: CanViewApplicants, CanAddApplicants
+    Urls: /applicants/ and /applicants/<uid>/
     """
-    fixtures = ["groups.json"]
+
     url = reverse("applicant-list")
-    client = APIClient()
 
     def setUp(self):
         self.username_can_view = "can_view_applicants"
         self.username_no_permissions = "no_permissions"
         self.username_can_view_add = "can_view_add_applicants"
-        self.email = "email@email.com"
-        self.password = "test-password"
 
         # can view applicants, not ADD
         self.user_can_view_applicants = self.create_test_user(
@@ -44,12 +53,12 @@ class TestGetCreateApplicant(ATSAPITestCase):
             password=self.password,
             group_names=["CanViewApplicants"],
         )
-        
+
         # no permissions
         self.user_no_permissions = self.create_test_user(
             self.username_no_permissions, self.email, self.password
         )
-        
+
         # can add and view applicants
         self.user_can_view_add_applicants = self.create_test_user(
             username=self.username_can_view_add,
@@ -57,34 +66,139 @@ class TestGetCreateApplicant(ATSAPITestCase):
             password=self.password,
             group_names=["CanAddApplicants", "CanViewApplicants"],
         )
+        
+        self.test_applicant = Applicant.objects.create(name="test applicant")
 
     def test_get_applicant_list_unauthenticated(self):
+        """Check that unauthenticated users cannot view the applicant list."""
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 403)
 
+    def test_get_applicant_list_no_permissions(self):
+        """Checks that a user with no permissions cannot view the applicant list."""
+        self.client.login(username=self.username_no_permissions, password=self.password)
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.post(self.url, {"name": "test applicant"})
+        self.assertEqual(response.status_code, 403)
+
+
     def test_get_applicant_list_can_view_applicants(self):
+        """Checks that a user with the CanViewApplicants permission can view the applicant list."""
         self.client.login(username=self.username_can_view, password=self.password)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
 
-    def test_get_applicant_list_no_permissions(self):
-        self.client.login(username=self.username_no_permissions, password=self.password)
-        
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 403)
-
-        response = self.client.post(self.url, {"name": "test applicant"})
-        self.assertEqual(response.status_code, 403)
-
     def test_add_applicant_can_add_applicants(self):
-        self.client.login(username=self.user_can_view_add_applicants, password=self.password)
-        
+        """Checks that a user with the CanAddApplicants permission can add an applicant."""
+        self.client.login(
+            username=self.user_can_view_add_applicants, password=self.password
+        )
         response = self.client.post(self.url, {"name": "test applicant"})
         self.assertEqual(response.status_code, 201)
         self.assertIn("uid", response.data)
         uid = response.data["uid"]
-        
+
+        updated_applicant = Applicant.objects.get(uid=uid)
+        self.assertEqual(updated_applicant.status, PENDING)
+
         url_detail = reverse("applicant-detail", kwargs={"pk": uid})
         response = self.client.get(url_detail)
         self.assertEqual(response.status_code, 200)
 
+
+class TestUpdateApplicant(ATSAPITestCase):
+    """Tests the ApplicantViewSet for PATCH requests.
+    Checks that users with the correct permissions can approve, reject, and update notes for applicants.
+    Permissions tested: CanApproveApplicants, CanUpdateNote
+    Urls: /applicants/<uid>/approve/, /applicants/<uid>/reject/, /applicants/<uid>/note/
+    """
+
+    def setUp(self):
+        self.username_can_approve = "can_approve"
+        self.username_no_permissions = "no_permissions"
+        self.username_can_update_note = "can_update_note"
+
+        # can approve applicants
+        self.user_can_approve = self.create_test_user(
+            username=self.username_can_approve,
+            email=self.email,
+            password=self.password,
+            group_names=["CanApproveApplicants"],
+        )
+
+        # can update note only
+        self.user_can_update_note = self.create_test_user(
+            username=self.username_can_update_note,
+            email=self.email,
+            password=self.password,
+            group_names=["CanUpdateNote"],
+        )
+
+        # no permissions at all
+        self.user_no_permissions = self.create_test_user(
+            self.username_no_permissions, self.email, self.password
+        )
+        
+        self.test_applicant = Applicant.objects.create(name="test applicant")
+
+    def test_update_applicant_unauthenticated(self):
+        """Shouldnt be able to do anything if not authenticated so only 403s"""
+        url = reverse("applicant-approve", kwargs={"pk": self.test_applicant.uid})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 403)
+
+        url = reverse("applicant-reject", kwargs={"pk": self.test_applicant.uid})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 403)
+
+        url = reverse("applicant-note", kwargs={"pk": self.test_applicant.uid})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_update_applicant_no_permissions(self):
+        """Checks authenticated user with no permissions cannot approve, reject, or update notes."""
+        self.client.login(username=self.username_no_permissions, password=self.password)
+
+        url = reverse("applicant-approve", kwargs={"pk": self.test_applicant.uid})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 403)
+
+        url = reverse("applicant-reject", kwargs={"pk": self.test_applicant.uid})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 403)
+
+        url = reverse("applicant-note", kwargs={"pk": self.test_applicant.uid})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_approve_reject_applicant_can_approve(self):
+        """Checks that a user with the CanApproveApplicants permission can approve and reject applicants."""
+        self.client.login(username=self.username_can_approve, password=self.password)
+
+        url = reverse("applicant-approve", kwargs={"pk": self.test_applicant.uid})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 202)
+        updated_applicant = Applicant.objects.get(uid=self.test_applicant.uid)
+        self.assertEqual(updated_applicant.status, APPROVED)
+
+        url = reverse("applicant-reject", kwargs={"pk": self.test_applicant.uid})
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, 202)
+        updated_applicant = Applicant.objects.get(uid=self.test_applicant.uid)
+        self.assertEqual(updated_applicant.status, REJECTED)
+
+    def test_update_note_applicant_can_update(self):
+        """Checks that a user with the CanUpdateNote permission can update notes"""
+        self.client.login(
+            username=self.username_can_update_note, password=self.password
+        )
+
+        url = reverse("applicant-note", kwargs={"pk": self.test_applicant.uid})
+        test_note = "updated note"
+        response = self.client.patch(url, {"note": test_note})
+        self.assertEqual(response.status_code, 202)
+        updated_applicant = Applicant.objects.get(uid=self.test_applicant.uid)
+        self.assertEqual(updated_applicant.note, test_note)
